6436f73dea361c3b445f60bafe3617b1fa64ba88
Henning Kage <henning.kage@googlemail.com>
2013-06-14 10:53:13 +0200

    Changed test files

--- setup.py
+++ setup.py
@@ -12,8 +12,13 @@ setup(
     author_email='henning.kage@gmail.com',
     url='https://github.com/hkage/django-issue-synchronisation',
     license='GPLv3',
-    packages=find_packages(),
+    packages=find_packages(exclude=('tests',)),
     data_files=[('issues/fixtures', ['issues/fixtures/issues.json'])],
+    tests_require=[
+        'django>=1.3,<1.6',
+    ],
+    test_suite='runtests.runtests',
+    include_package_data=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
